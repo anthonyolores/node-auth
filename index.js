de049787f6f798d5adacac6e5a76c12c032aa6ef
anthonyolores <tonyoolores@gmail.com>
2018-06-05 00:34:28 +1200

    added fb oauth

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const app = express();
 const passport = require('passport');
 const port = process.env.PORT || 3000;
+const fb_app_id = 'your app id';
+const fb_secret = 'your fb secret';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,11 +44,21 @@ passport.use(new LocalStrategy(
         });
     }
   ));
+  passport.use(new FacebookStrategy({
+    clientID: fb_app_id,
+    clientSecret: fb_secret,
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+  }
+));
 
 /* HTTP Requests */
 app.get('/', (req, res) => res.sendFile('login.html', { root : __dirname}));
 app.get('/success', (req, res) => res.send(`Hello ${req.query.username}`));
 app.get('/error', (req, res) => res.send("Invalid User!!"));
+app.get('/auth/facebook', passport.authenticate('facebook'));
 app.post('/',
   passport.authenticate('local', { 
     failureRedirect: '/error' }),
@@ -53,6 +66,11 @@ app.post('/',
         res.redirect('/success?username='+req.user.username);
     }
 );
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success?username='+req.user.displayName);
+  });
 
 /* Set Mongoose*/
 mongoose.connect('mongodb://localhost:27017/nodeauth');
